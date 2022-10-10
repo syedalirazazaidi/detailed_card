b35ce740e3d2fca55811d4acade42881ccde18c2
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-10-11 00:20:32 +0500

    Add project001

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
+import React from 'react';
+import {
+  Heading,
+  Text,
+  Image,
+  Box,
+  Flex,
+  HStack,
+  VStack,
+  Divider,
+} from '@chakra-ui/react';
 
-import { Container } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react';
 const Cart = () => {
   return (
-    <Container w='640px' bg='green.100'>
-dsfds
+    <Container w="640px" bg="#F7FAFC" py="40px">
+      <Heading>Your cart</Heading>
+      <Text>If price is too hard on your eyes, try changing the theme.</Text>
+      <Flex justifyContent="space-between" px="20px">
+        <HStack>
+          <Image
+            boxSize="100px"
+            objectFit="cover"
+            src="https://bit.ly/dan-abramov"
+            alt="Dan Abramov"
+          />
+          <VStack>
+            <Text>Penny board</Text>
+            <Text>PNYCOMP27541</Text>
+          </VStack>
+        </HStack>
+        <Box>
+          <Text>$119.00</Text>
+        </Box>
+      </Flex>
+      <Flex justifyContent="space-between">
+        <Text>Subtotal</Text>
+        <Text>$119.00</Text>
+      </Flex>
+      <Flex justifyContent="space-between">
+        <Text>Shipping</Text>
+        <Text>$19.99</Text>
+      </Flex>
+      <Flex justifyContent="space-between">
+        <Text>Taxes (estimated)</Text>
+        <Text>$23.80</Text>
+      </Flex>
+      <Divider orientation="horizontal" />
+      <Flex justifyContent="space-between">
+        <Text>Total</Text>
+        <Text>$162.79</Text>
+      </Flex>
     </Container>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -1,12 +1,69 @@
-import React from 'react'
-import { Container } from '@chakra-ui/react'
+import React from 'react';
+import {
+  Container,
+  Heading,
+  Text,
+  Flex,
+  SimpleGrid,
+  Box,
+  Input,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Checkbox,
+  Stack,
+  Button,
+  Select,
+} from '@chakra-ui/react';
 
 const Details = () => {
   return (
-    <Container w='640px' bg='red.100'>
-fdgf
+    <Container w="640px" padding="40px">
+      <Heading as="h1" fontSize="48px" fontWeight="700">
+        Your details
+      </Heading>
+      <Text>
+        <Text fontSize="16px" fontWeight="400">
+          If you already have an account, click here to log in.
+        </Text>
+      </Text>
+      <SimpleGrid columns={[1, null, 2]} spacing="40px">
+        <FormControl>
+          <FormLabel>First Name</FormLabel>
+          <Input type="text" placeholder="John" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Last Name</FormLabel>
+          <Input type="text" placeholder="Doe" />
+        </FormControl>
+      </SimpleGrid>
+      <SimpleGrid>
+        <FormControl>
+          <FormLabel>Address</FormLabel>
+          <Input type="John" placeholder="Blvd. Broken Dreams 21" />
+        </FormControl>
+      </SimpleGrid>
+      <SimpleGrid columns={[1, null, 2]} spacing="40px">
+        <FormControl>
+          <FormLabel>City</FormLabel>
+          <Input type="text" placeholder="San Francisco" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Country</FormLabel>
+          <Select placeholder="Select option">
+            <option value="option1">United States of America</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </Select>
+          {/* <Input type="text" placeholder="United States of America" /> */}
+        </FormControl>
+      </SimpleGrid>
+      <Stack>
+        <Checkbox defaultChecked>Ship to the billing address.</Checkbox>
+      </Stack>
+      <Button colorScheme="whatsapp">Place Order</Button>
     </Container>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
