1fca2fd92fd79da2635442b8ed07c5ffcf4cd30e
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-10-12 01:09:47 +0500

    add app completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { ChakraProvider, Box, Grid, Flex, theme } from '@chakra-ui/react';
+import { ChakraProvider, Box, Grid, Flex } from '@chakra-ui/react';
+import customTheme from './theme/customTheme';
+import './theme/styles.css'
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import Details from './pages/details';
 import Cart from './pages/cart';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={customTheme}>
       <Box textAlign="center" fontSize="xl">
         <Grid p={4}>
           <ColorModeSwitcher justifySelf="flex-end" />
--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {  useColorModeValue } from '@chakra-ui/react';
+import { useColorModeValue } from '@chakra-ui/react';
 import {
   Container,
   Heading,
@@ -17,45 +17,46 @@ import {
 } from '@chakra-ui/react';
 
 const Details = () => {
-    const bgColor = useColorModeValue('#1A202C', 'white.50');
+  const bgColor = useColorModeValue('#1A202C', 'white.50');
   return (
-    <Container w="640px" px="80px" mx={[-12,10,10]}  h="740px">
+    <Container w="640px" px="80px" mx={[-12, 10, 10]} h="740px">
       <Box textAlign="left" w="560px">
         <Heading as="h1" fontSize="48px" w="297px" fontWeight="700">
           Your details
         </Heading>
-        <Text fontSize={["13px","16px","16px"]} fontWeight="400" mb="40px" color={bgColor}>
+        <Text
+          fontSize={['13px', '16px', '16px']}
+          fontWeight="400"
+          mb="40px"
+          color={bgColor}
+        >
           If you already have an account, click here to log in.
         </Text>
       </Box>
       <SimpleGrid columns={[1, null, 2]} spacing="40px" paddingBottom="24px">
-        <FormControl w={["290px","242px","242px"]}>
+        <FormControl w={['290px', '242px', '242px']}>
           <FormLabel>First Name</FormLabel>
-          <Input type="text" placeholder="John" variant="filled" />
+          <Input type="text" placeholder="John" />
         </FormControl>
-        <FormControl  w={["290px","242px","242px"]} border="none">
+        <FormControl w={['290px', '242px', '242px']} border="none">
           <FormLabel>Last Name</FormLabel>
-          <Input type="text" placeholder="Doe" variant="filled" />
+          <Input type="text" placeholder="Doe" />
         </FormControl>
       </SimpleGrid>
       <SimpleGrid>
-        <FormControl w={["290px","502px","502px"]} py="24px">
+        <FormControl w={['290px', '502px', '502px']} py="24px">
           <FormLabel>Address</FormLabel>
-          <Input
-            type="John"
-            placeholder="Blvd. Broken Dreams 21"
-            variant="filled"
-          />
+          <Input type="John" placeholder="Blvd. Broken Dreams 21" />
         </FormControl>
       </SimpleGrid>
       <SimpleGrid columns={[1, null, 2]} spacing="40px" py="24px">
-        <FormControl  w={["290px","242px","242px"]}>
+        <FormControl w={['290px', '242px', '242px']}>
           <FormLabel>City</FormLabel>
-          <Input type="text" placeholder="San Francisco" variant="filled" />
+          <Input type="text" placeholder="San Francisco" />
         </FormControl>
-        <FormControl w={["290px","242px","242px"]}>
+        <FormControl w={['290px', '242px', '242px']}>
           <FormLabel>Country</FormLabel>
-          <Select  variant="filled">
+          <Select variant="filled">
             <option value="usa">United States of America</option>
             <option value="uae">United Arab Emirates</option>
             <option value="uk"> United Kingdom</option>
@@ -65,11 +66,16 @@ const Details = () => {
       </SimpleGrid>
 
       <Stack py="5px">
-        <Checkbox size="lg" colorScheme="green" defaultChecked py="5px">
+        <Checkbox size="lg" defaultChecked py="5px">
           Ship to the billing address.
         </Checkbox>
       </Stack>
-      <Button bg="#8AC919" color="white" marginRight={['230px','20px','0px']} w={["290px","502px","502px"]}>
+      <Button
+        variant="primary"
+      
+        marginRight={['230px', '20px', '0px']}
+        w={['290px', '502px', '502px']}
+      >
         Place Order
       </Button>
     </Container>
